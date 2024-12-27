f240e682b0b6e8db6f3b28553f60464b0ffe4343
mj <minai621@naver.com>
2024-12-28 07:06:55 +0900

    fix typo

--- src/lib/pages/generate/components/step-five-five.tsx
+++ src/lib/pages/generate/components/step-five-five.tsx
@@ -110,7 +110,7 @@ const FileUploadButton = () => {
     <FileUploadRoot>
       <FileUploadTrigger asChild>
         <Button variant="outline" size="sm" as="label" disabled={isUploading}>
-          <HiUpload /> {isUploading ? 'Uploading...' : 'Upload files'}
+          <HiUpload /> {isUploading ? '업로드중...' : '파일 업로드'}
           <input
             type="file"
             multiple
